fdb95fa51bbd4075557cb2aba3e82525231fe563
RaneemAbdulazez <raneem97su@gmail.com>
2021-05-04 01:02:32 +0300

    done

--- python/challenges/array_shift/README.md
+++ python/challenges/array_shift/README.md
@@ -1,12 +1,14 @@
 # Challenge Summary
 <!-- Short summary or background information -->
-this challenge requires to add an 
-
+we were asked to insert a value inside a middle of a list
 ## Challenge Description
 <!-- Description of the challenge -->
+this challenge requires to add a value inside the middle of the given list without using the built in
 
 ## Approach & Efficiency
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Big O(n)
+space O(n)
 
 ## Solution
 <!-- Embedded whiteboard image -->
--- python/tests/test_array_shift.py
+++ python/tests/test_array_shift.py
@@ -7,25 +7,36 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
-def test_array_shift():
+def test_array_shift_evenlength():
 
     #arrange
     expected=[2,4,5,6,8]
-    expected2=[4,8,15,16,23,42]
-    expected3='plz enter valid inputs'
-
-
     actual=insertShiftArray([2,4,6,8], 5)
+    assert expected==actual
+
+
+
+
+
+def test_array_shift_oddlength():
+    expected2=[4,8,15,16,23,42]
     actual2=insertShiftArray([4,8,15,23,42], 16	)
+    assert expected2==actual2
+
+
+
+
+def test_array_shift_faildtest():
+
+    expected3='plz enter valid inputs'
     actual3=insertShiftArray([], 16	)
     actual4=insertShiftArray([4,8,15,23,42], 'val')
-
-
-    #assert
-    assert expected==actual
-    assert expected2==actual2
     assert expected3==actual3
     assert expected3==actual4
+
+
+
+
 
 
 
