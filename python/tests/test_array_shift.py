b3fe348483108fa09eb1856b83c7c0ca94384837
RaneemAbdulazez <raneem97su@gmail.com>
2021-05-04 00:51:12 +0300

    done

--- python/tests/test_array_shift.py
+++ python/tests/test_array_shift.py
@@ -8,4 +8,29 @@ def test_version():
 
 
 def test_array_shift():
-    
+
+    #arrange
+    expected=[2,4,5,6,8]
+    expected2=[4,8,15,16,23,42]
+    expected3='plz enter valid inputs'
+
+
+    actual=insertShiftArray([2,4,6,8], 5)
+    actual2=insertShiftArray([4,8,15,23,42], 16	)
+    actual3=insertShiftArray([], 16	)
+    actual4=insertShiftArray([4,8,15,23,42], 'val')
+
+
+    #assert
+    assert expected==actual
+    assert expected2==actual2
+    assert expected3==actual3
+    assert expected3==actual4
+
+
+
+
+
+
+
+
