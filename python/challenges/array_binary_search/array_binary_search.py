94f2a6f72cb0f260132070526229aabd3fa7a9ce
RaneemAbdulazez <raneem97su@gmail.com>
2021-05-04 22:52:33 +0300

    done

---
+++ python/challenges/array_binary_search/array_binary_search.py
@@ -0,0 +1,10 @@
+
+def binary_search(l, key):
+    if key not in l:
+        return -1
+    else:
+        x = 0
+        while x< len(l):
+            if key == l[x]:
+                return i
+            i += 1
