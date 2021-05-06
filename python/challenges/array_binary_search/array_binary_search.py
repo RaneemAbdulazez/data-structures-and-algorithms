4412e5635abc75ad43f829881bdd7f38ff5a1018
RaneemAbdulazez <raneem97su@gmail.com>
2021-05-06 23:35:40 +0300

    linked-lists

--- python/challenges/array_binary_search/array_binary_search.py
+++ python/challenges/array_binary_search/array_binary_search.py
@@ -1,10 +1,40 @@
+import math
+def binary_search(lst, value):
+    new_list =lst
+    mid=midpint(len(new_list))
 
-def binary_search(l, key):
-    if key not in l:
-        return -1
+
+    while  len(new_list) :
+
+        if value==new_list[mid]:
+            return mid
+
+        if value > new_list[mid]:
+            new_list = new_list[mid:]
+            mid=midpint(len(new_list))
+
+
+        elif value < new_list[mid]:
+            new_list = new_list[:mid]
+
+
+            mid=midpint(len(new_list))
+        else:
+            return -1
+
     else:
-        x = 0
-        while x< len(l):
-            if key == l[x]:
-                return i
-            i += 1
+        return 'empty'
+
+
+
+def midpint(len2):
+    if not len2 % 2:
+        mid = len2 // 2
+
+    else:
+        mid = math.ceil(len2 // 2)
+
+    return mid
+
+
+print(binary_search([1,2,3,4,5], 2))
